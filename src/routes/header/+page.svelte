baf0af23cda762158f8487672d9f18ef02e649da
HotBurgi <mtorri2007@gmail.com>
2025-10-13 10:34:42 +0200

    Layout base header + form task

---
+++ src/routes/header/+page.svelte
@@ -0,0 +1,30 @@
+<div>
+    <h1>Hello user!</h1>
+    <p>Welcome back!</p>
+    
+    <h1>TRM</h1>
+</div>
+
+
+<div>
+    <div>
+        <h1>TOTAL</h1>
+        <h1>10</h1>
+    </div>
+
+    <div>
+        <h1>IN PROGRESS</h1>
+        <h1>20</h1>
+    </div>
+
+    <div>
+        <h1>COMPLETED</h1>
+        <h1>30</h1>
+    </div>
+</div>
+
+
+<form>
+    <input type="text" placeholder="Search.." name="search">
+    <button type="submit"></button>
+</form>
