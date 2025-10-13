3bf965e8a12111a818356dbd567739d6a8be1e73
HotBurgi <mtorri2007@gmail.com>
2025-10-13 10:15:05 +0200

    Creato pagina form

--- src/routes/header/+page.svelte
+++ src/routes/header/+page.svelte
@@ -32,24 +32,3 @@
     <input type="text" placeholder="Search.." name="search">
     <button type="submit"></button>
 </form>
-
-<form>
-    <label for="title">Title:</label>
-    <input type="text" id="title" name="title"><br>
-
-    <label for="object">Object:</label>
-    <input type="text" id="object" name="object"><br>
-  
-    <label for="deadline">Deadline:</label>
-    <input type="date" id="deadline" name="deadline"><br>
-    
-    <label for="priority">Priority:</label>
-    <select id="priority" name="priority">
-        <option value="Urgent">Urgent</option>
-        <option value="Medium">Medium</option>
-        <option value="Low">Low</option>
-        <option value="None">None</option>
-    </select><br>
-
-    <input type="submit" value="Submit">
-</form>
